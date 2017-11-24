f6e1288485c1a8deb056b9721a9989fcb3b14cb8
Mattu <xuniqh@gmail.com>
2017-11-24 14:29:58 +0100

    fxml filer

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("forside.fxml"));
         primaryStage.setTitle("LMBank");
-        primaryStage.setScene(new Scene(root, 700, 575));
+        primaryStage.setScene(new Scene(root, 1280, 809));
         primaryStage.show();
     }
 
