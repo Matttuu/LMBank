fdcab354f33025c1d1c7f466b9ffe823d19e1116
Sebastian Ougter Olsen <seba3440@edu.easj.dk>
2017-12-04 09:42:44 +0100

    Opret konto GUI

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("GUI/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/OpretKonto.fxml"));
         primaryStage.setTitle("LMBank");
         primaryStage.setScene(new Scene(root, 600, 463));
         primaryStage.show();
